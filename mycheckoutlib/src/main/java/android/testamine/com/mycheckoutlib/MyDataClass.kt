2aeebb20a54e7cfb214591e8aa5aed721bceeb38
Rihane, Amine <medamine.rihane@hotmail.com>
2018-04-09 14:21:40 +0200

    Initial commit

---
+++ mycheckoutlib/src/main/java/android/testamine/com/mycheckoutlib/MyDataClass.kt
@@ -0,0 +1,11 @@
+package android.testamine.com.mycheckoutlib
+
+class MyDataClass {
+    companion object {
+
+        fun div10(mynum: Int): Int {
+            return mynum / 10
+        }
+    }
+
+}
